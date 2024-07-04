3fe17ad5b8025e8f4473e590f569c6655d56a316
Kriglz <Kriglz@yandex.ru>
2024-07-04 10:49:22 +0300

    Client

---
+++ ewm-stats-service/stats-client/src/main/java/StatsClient.java
@@ -0,0 +1,41 @@
+import EndpointHitDto.EndPointHitDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class StatsClient extends BaseClient{
+
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
+    @Autowired
+    public StatsClient(String serverUrl, RestTemplateBuilder builder) {
+        super(builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                .requestFactory(HttpComponentsClientHttpRequestFactory::new)
+                .build());
+    }
+
+    public void addHit(EndPointHitDto endPointHitDto) {
+        post("/hit", endPointHitDto);
+    }
+
+    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, List<String> uris,
+                                           Boolean unique) {
+        Map<String, Object> parameters = Map.of(
+                "start", start.format(dateTimeFormatter),
+                "end", end.format(dateTimeFormatter),
+                "uris", String.join(",", uris),
+                "unique", unique
+        );
+        return get("/stats?start={start}&end={end}&uris={uris}&unique={unique}", parameters);
+    }
+}
