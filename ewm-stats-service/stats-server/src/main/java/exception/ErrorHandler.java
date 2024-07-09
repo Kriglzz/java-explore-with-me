bdc2a02228bc0f87dd52053dfc88fa511f17bc8e
Kriglz <Kriglz@yandex.ru>
2024-07-09 23:28:56 +0300

    exception

---
+++ ewm-stats-service/stats-server/src/main/java/exception/ErrorHandler.java
@@ -0,0 +1,19 @@
+package exception;
+
+
+import controller.StatsController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice(assignableTypes = {StatsController.class})
+public class ErrorHandler {
+    @ExceptionHandler(ValidationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationException(final ValidationException e) {
+        return Map.of("error", e.getMessage());
+    }
+}
